4702c8dd98dc4d9ec543db10b2a31b7cc38bdb0a
Caio Guedes <caio.cesar.g.souza@gmail.com>
2018-06-24 15:49:13 -0300

    Project template

---
+++ test/guestbook_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns guestbook-api.core-test
+  (:require [clojure.test :refer :all]
+            [guestbook-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
